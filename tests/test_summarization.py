57b5b61cd6b36dd72585826e5ce312d27a46bce4
tontoroRR <sabuaka0123@proton.me>
2024-04-20 10:55:05 +0900

    change sounds, HQ max another img, edit test code for summarization

--- tests/test_summarization.py
+++ tests/test_summarization.py
@@ -1,8 +1,28 @@
-# from modules.rushroyale_stats import Hero, Unit, RushRoyaleStats
-# import yaml
+from modules.rushroyale_stats import RushRoyaleStats
+import yaml
+import pytest
+
+
+@pytest.fixture
+def decks():
+    with open('tests/resources/summarize1.yml', 'r') as _yml:
+        decks = yaml.safe_load(_yml)['decks']
+    return decks
+
+
+@pytest.fixture
+def stats():
+    rrs = RushRoyaleStats()
+    rrs.setup()
+    return rrs
 
 
 class TestSummarization:
-    def test__summarize_deck(self):
-        # deck =
-        assert 1 == 1
+    def test__load_deck_yaml(self, decks):
+        assert 10 == len(decks)
+
+    def test__apply_unit_stats(self, decks, stats):
+        pass
+
+    def test__summarize_deck(self, decks):
+        assert 10 == len(decks)
